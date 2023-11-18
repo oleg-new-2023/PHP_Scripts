ad231244ed981f56a8d593e440cd3f1c2e02cd0e
oleg <771900@gmail.com>
2023-11-18 23:40:36 +0200

    add file "Json_Array_Parser"

---
+++ Json_Array_Parser.php
@@ -0,0 +1,9 @@
+<?php
+$sour_string = '{"status":"success","message":"","listAppRequest":[{"id":6080,"address":"м. Одеса, Космонавтів, ⁶8, кв. 154"}],"users":{"1":{"login":"ID40714","address":" Академика Королева 72а, 1","accounts":[{"id":40714,"address":" Академика Королева 72а, 1"}]},"2":{"login":"ID44835","address":" Академика Королева 92а","accounts":[{"id":44835,"address":" Академика Королева 92а"}]},"3":{"login":"ID49524","address":" Владимира Высоцкого 11","accounts":[{"id":49524,"address":" Владимира Высоцкого 11"}]},"4":{"login":"ID54230","address":" Александра Невского 8","accounts":[{"id":54230,"address":" Александра Невского 8"}]}}}';
+$ids = array();
+
+
+
+
+
+?>
